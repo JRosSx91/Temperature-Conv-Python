0f86731dbea998817a10d76d205b68b348d08cc9
JRosSx91 <hazee.1491@gmail.com>
2023-05-30 16:27:12 +0200

    add message function

--- main.py
+++ main.py
@@ -20,3 +20,14 @@ def converter_fahr_to_rankine(x):
 
 def converter_rankine_to_fahr(x):
     return x - 459.67
+
+
+def print_temp_message(temp, scale):
+    message = {
+        "Celsius": "That's really cold!❄️" if temp < 0.0 else "That's really hot!🔥" if temp >= 30.0 else "That's a moderate temperature.",
+        "Fahrenheit": "That's really cold!❄️" if temp < 32.0 else "That's really hot!🔥" if temp >= 86.0 else "That's a moderate temperature.",
+        "Kelvin": "That's really cold!❄️" if temp < 273.15 else "That's really hot!🔥" if temp >= 303.15 else "That's a moderate temperature.",
+        "Rankine": "That's really cold!❄️" if temp < 491.67 else "That's really hot!🔥" if temp >= 545.67 else "That's a moderate temperature.",
+    }.get(scale, "Invalid scale.")
+    print(
+        f"The converted temperature is {temp:.2f} degrees {scale}. {message}")
