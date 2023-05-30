fb68550ab50223fedce32ba8929c83feaa068d2f
JRosSx91 <hazee.1491@gmail.com>
2023-05-30 16:29:18 +0200

    add main function

--- main.py
+++ main.py
@@ -31,3 +31,54 @@ def print_temp_message(temp, scale):
     }.get(scale, "Invalid scale.")
     print(
         f"The converted temperature is {temp:.2f} degrees {scale}. {message}")
+
+
+def main():
+    print("Welcome to Fº/Cº/K/R converter!")
+    print("Please enter the temperature you want to convert:")
+    temp = float(input())
+
+    print("What scale is this temperature in? Enter 'F' for Fahrenheit, 'C' for Celsius, 'K' for Kelvin and 'R' for Rankine:")
+    scale = input().strip().upper()
+
+    if scale == "F":
+        print("Converting to Celsius, Rankine and Kelvin:")
+        celsius = converter_cel(temp)
+        rankine = converter_fahr_to_rankine(temp)
+        kelvin = converter_cel_to_kelvin(celsius)
+        print_temp_message(celsius, "Celsius")
+        print_temp_message(rankine, "Rankine")
+        print_temp_message(kelvin, "Kelvin")
+    elif scale == "C":
+        print("Converting to Fahrenheit, Kelvin and Rankine:")
+        fahr = converter_fahr(temp)
+        rankine = converter_fahr_to_rankine(fahr)
+        kelvin = converter_cel_to_kelvin(temp)
+        print_temp_message(fahr, "Fahrenheit")
+        print_temp_message(kelvin, "Kelvin")
+        print_temp_message(rankine, "Rankine")
+    elif scale == "R":
+        print("Converting to Fahrenheit, Celsius and Kelvin:")
+        fahr = converter_rankine_to_fahr(temp)
+        celsius = converter_cel(fahr)
+        kelvin = converter_cel_to_kelvin(celsius)
+        print_temp_message(fahr, "Fahrenheit")
+        print_temp_message(celsius, "Celsius")
+        print_temp_message(kelvin, "Kelvin")
+    elif scale == "K":
+        print("Converting to Celsius, Fahrenheit and Rankine:")
+        celsius = converter_kelvin_to_cel(temp)
+        fahr = converter_fahr(celsius)
+        rankine = converter_fahr_to_rankine(fahr)
+        print_temp_message(celsius, "Celsius")
+        print_temp_message(fahr, "Fahrenheit")
+        print_temp_message(rankine, "Rankine")
+    else:
+        print("Invalid scale entered.")
+        return
+
+    input("\nPress Enter to exit...")
+
+
+if __name__ == "__main__":
+    main()
